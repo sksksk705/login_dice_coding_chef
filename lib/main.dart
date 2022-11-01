62b3a41546bf43243d3a5485ca96e6466cfe9677
unknown <sksksk705@hanyang.ac.kr>
2022-11-01 21:41:48 +0900

    4강

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dice.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -19,6 +21,8 @@ class LogIn extends StatefulWidget {
 }
 
 class _LogInState extends State<LogIn> {
+  TextEditingController _controller = TextEditingController();
+  TextEditingController _controller2 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,62 +35,124 @@ class _LogInState extends State<LogIn> {
           IconButton(icon: Icon(Icons.search), onPressed: () {})
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.only(top: 50)),
-            Center(
-              child: Image(
-                image: AssetImage(
-                  'image/chef.gif',
-                ),
-                width: 170.0,
-                height: 190.0,
-              ),
-            ),
-            Form(
-                child: Theme(
-              data: ThemeData(
-                  primaryColor: Colors.teal,
-                  inputDecorationTheme: InputDecorationTheme(
-                      labelStyle:
-                          TextStyle(color: Colors.teal, fontSize: 15.0))),
-              child: Container(
-                padding: EdgeInsets.all(40.0),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Enter 'dice'",
-                      ),
-                      keyboardType: TextInputType.emailAddress,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        labelText: "Enter Password",
-                      ),
-                      keyboardType: TextInputType.text,
-                      obscureText: true,
-                    ),
-                    SizedBox(
-                      height: 40,
-                    ),
-                    ButtonTheme(
-                        minWidth: 100.0,
-                        height: 50.0,
-                        child: ElevatedButton(
-                            onPressed: () {},
-                            child: Icon(
-                              Icons.arrow_forward,
-                              color: Colors.white,
-                              size: 35.0,
-                            )))
-                  ],
+      body: Builder(builder: (context) {
+        return SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 50)),
+              Center(
+                child: Image(
+                  image: AssetImage(
+                    'image/chef.gif',
+                  ),
+                  width: 170.0,
+                  height: 190.0,
                 ),
               ),
-            )),
-          ],
+              Form(
+                  child: Theme(
+                data: ThemeData(
+                    primaryColor: Colors.teal,
+                    inputDecorationTheme: InputDecorationTheme(
+                        labelStyle:
+                            TextStyle(color: Colors.teal, fontSize: 15.0))),
+                child: Container(
+                  padding: EdgeInsets.all(40.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          labelText: "Enter 'dice'",
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      TextField(
+                        controller: _controller2,
+                        decoration: InputDecoration(
+                          labelText: "Enter Password",
+                        ),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      ButtonTheme(
+                          minWidth: 100.0,
+                          height: 50.0,
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.orangeAccent),
+                              onPressed: () {
+                                if (_controller.text == "dice" &&
+                                    _controller2.text == '1234') {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (BuildContext context) =>
+                                              Dice()));
+                                } else if (_controller.text == "dice" &&
+                                    _controller2.text != "1234") {
+                                  showSnackBar2(context);
+                                } else if (_controller.text != "dice" &&
+                                    _controller2.text == "1234") {
+                                  showSnackBar3(context);
+                                } else {
+                                  showSnackBar(context);
+                                }
+                              },
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                                size: 35.0,
+                              )))
+                    ],
+                  ),
+                ),
+              )),
+            ],
+          ),
+        );
+      }),
+    );
+  }
+
+  void showSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          "로그인 정보를 다시 확인하세요",
+          textAlign: TextAlign.center,
         ),
+        duration: Duration(seconds: 2),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+
+  void showSnackBar2(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          "비밀번호가 일치하지 않습니다",
+          textAlign: TextAlign.center,
+        ),
+        duration: Duration(seconds: 2),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+
+  void showSnackBar3(BuildContext context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          "dice의 철자를 확인하세요",
+          textAlign: TextAlign.center,
+        ),
+        duration: Duration(seconds: 2),
+        backgroundColor: Colors.blue,
       ),
     );
   }
